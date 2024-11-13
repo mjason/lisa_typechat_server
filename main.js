e71b15480637a8574d6f73bbe78ac851adca17c4
MJ <tywf91@gmail.com>
2024-11-13 16:04:09 +0800

    加上日志

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ import { validateSchema, generatePrompt, jsonLoader } from "./llm.js";
 const app = new Application();
 const router = new Router();
 
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
 
 router.post("/validate", async (ctx) => {
   try {
